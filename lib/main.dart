42b3d7da28b1cb396edce27558931ffadfb9cf6a
Owen Johnston <32434575+owjoh@users.noreply.github.com>
2019-02-21 21:03:16 +0000

    -Added the new home screen which gives an overview of your personal steps and no score or multipliers -Started messing around with Animations, but have commented it all out -

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 import 'package:pedometer/pedometer.dart';
 import 'dart:async';
+import 'dart:math';
 
 void main() {
   runApp(new MaterialApp(home: new Scaffold(body: new SamplePage())));
@@ -17,12 +18,119 @@ bool isCompleted = false;
 Color blue = Colors.blue;
 Color blueDark = Colors.blue[700];
 TimeOfDay now = TimeOfDay.now();
+int _score = 0;
+int _steps = 0;
+int _multiplier = 1;
 
 
 
-
-class _SamplePageState extends State<SamplePage> {
+class _SamplePageState extends State<SamplePage> with TickerProviderStateMixin{
   int _currentIndex = 1;
+
+  //Animation
+  //-----------------------------------------------
+//  final duration = new Duration(milliseconds: 300);
+//  Timer timer;
+//  int _counter = 0;
+//  double _sparklesAngle = 0.0;
+//  Animation sparklesAnimation;
+//  AnimationController sparklesAnimationController;
+//  Random random;
+//
+//
+//  initState() {
+//    super.initState();
+//    sparklesAnimationController = new AnimationController(vsync: this, duration: duration);
+//    sparklesAnimation = new CurvedAnimation(parent: sparklesAnimationController, curve: Curves.easeIn);
+//    sparklesAnimation.addListener((){
+//      setState(() { });
+//    });
+//  }
+//
+//  dispose() {
+//    super.dispose();
+//  }
+//
+//
+//
+//  void increment(Timer t) {
+//    sparklesAnimationController.forward(from: 0.0);
+//    setState(() {
+//      _counter++;
+//      _sparklesAngle = random.nextDouble() * (2*pi);
+//    });
+//  }
+//
+//  Widget widgetAnimation() {
+//    var stackChildren = <Widget>[
+//    ];
+//
+//    var firstAngle = _sparklesAngle;
+//    var sparkleRadius = (sparklesAnimationController.value * 50);
+//    var sparklesOpacity = (1 - sparklesAnimation.value);
+//
+//
+//    for (int i = 0; i < 5; ++i) {
+//      var currentAngle = (firstAngle + ((2 * pi) / 5) * (i));
+//      var sparklesWidget =
+//      new Positioned(
+//        child:
+//        new Transform.rotate(
+//            angle: currentAngle - pi / 2,
+//            child:
+//            new Opacity(
+//                opacity: sparklesOpacity,
+//                child:
+//                new Image.asset(
+//                  "images/sparkles.png",
+//                  width: 14.0,
+//                  height: 14.0,
+//                )
+//            )
+//        ),
+//        left: (sparkleRadius * cos(currentAngle)) + 20,
+//        top: (sparkleRadius * sin(currentAngle)) + 20,
+//      );
+//      stackChildren.add(sparklesWidget);
+//    }
+//
+//    var widget = new Positioned(
+//        child: new Stack(
+//          alignment: FractionalOffset.center,
+//          overflow: Overflow.visible,
+//          children: stackChildren,
+//        )
+//        ,
+//        bottom: 5
+//    );
+//    return widget;
+//  }
+
+  AnimationController _controller;
+  Animation _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller =
+        AnimationController(vsync: this, duration: Duration(seconds: 2));
+
+    _animation = Tween(begin: -1.0, end: 0.0).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.fastOutSlowIn,
+    ));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+  //-----------------------------------------------
+  //Animation
+
+
 
   StreamSubscription<int> _subscription;
 
@@ -34,6 +142,7 @@ class _SamplePageState extends State<SamplePage> {
       ),
     );
   }
+
 
   String currentLeagueName = "SWENG 37";
   int steps = 0;
@@ -85,13 +194,13 @@ class _SamplePageState extends State<SamplePage> {
       )
     ];
     setUpPedometer();
-    Widget home = sampleHome(currentLeagueName, steps, goal, leaderboard, totalSteps);
+    Widget home = homePage(steps, goal);
     Widget history = historyPage();
-    Widget leagues = leaguesPage();
+    Widget leagues = leaguesPage(currentLeagueName, steps, goal, leaderboard, totalSteps);
     _screens[1] = home;
     _screens[0] = history;
     _screens[2] = leagues;
-
+    final double width = MediaQuery.of(context).size.width;
 
     return Scaffold(
       appBar: new AppBar(
@@ -105,16 +214,19 @@ class _SamplePageState extends State<SamplePage> {
               child:
               Text("RESET STEPS"),
             ),
-          )
+          ),
         ],
       ),
-      body: _screens[_currentIndex],
+      body:  _screens[_currentIndex],
+
+
       bottomNavigationBar:
       BottomNavigationBar(
         currentIndex: _currentIndex,
         onTap: (int index) {
           setState(() {
             _currentIndex = index;
+            //increment(null);
           });
         },
         items: [
@@ -245,9 +357,76 @@ CircularPercentIndicator mainIndicator(int steps, int goal) {
 
 }
 
+CircularPercentIndicator homeIndicator(int steps, int goal) {
+  return new CircularPercentIndicator(
+      startAngle: 180,
+      radius: RADIUS,
+      lineWidth: 15,
+      animation: true,
+      percent: steps < goal ? steps / goal : 1,
+      center: Column(
+        children: <Widget>[
+          new Padding(
+            padding: EdgeInsets.symmetric(vertical: 40),
+           ),
+          new Text(
+            "STEPS",
+            style: TextStyle(fontSize: 18, color: Colors.grey.withOpacity(0.5), fontWeight: FontWeight.bold,),
+          ),
+          new Text(
+            steps.toString(),
+            style: TextStyle(fontSize: 50, color: isCompleted ? Colors.lightGreenAccent[700] : Colors.blue, fontWeight: FontWeight.bold,),
+          ),
+          Image.asset(
+            "images/steps_image.png",
+            height: 60,
+            width: 50,
+            color: isCompleted ? Colors.lightGreenAccent[700] : Colors.blue,
+          ),
+        ],
+      ),
+      footer: Column(
+        children: <Widget>[
+          new Padding(
+              padding: EdgeInsets.symmetric(vertical: 7)
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(
+              'images/goal_image.png',
+               height: 30,
+               width: 25,
+                
+              ),
+              new Padding(
+                padding: EdgeInsets.symmetric(horizontal: 3),
+              ), 
+              new Text(
+                goal.toString(),
+                style: TextStyle(fontSize: 24, color: Colors.grey.withOpacity(0.8), fontWeight: FontWeight.bold,)
+              ),
+              new Padding(
+                padding: EdgeInsets.symmetric(horizontal: 3),
+              ),
+              Image.asset(
+                'images/steps_image.png',
+                height: 30,
+                width: 25,
+               ),
+            ],
+          ),
+        ],
+      ),
+      circularStrokeCap: CircularStrokeCap.round,
+      progressColor: isCompleted ? Colors.lightGreenAccent[400] : Colors.blue,
+      backgroundColor: Colors.grey.withOpacity(0.2),);
+
+}
+
 
 //Made this it's own entire widget so we can control what widgets are displayed(for changing screens)
-Widget sampleHome(String currentLeagueName, int steps, int goal, List<Widget> leaderboard, int totalSteps) {
+Widget leaguesFocus(String currentLeagueName, int steps, int goal, List<Widget> leaderboard, int totalSteps) {
   return (Center(
     child: ListView(
         children: <Widget>[
@@ -327,11 +506,42 @@ Widget historyPage() {
   );
 }
 
-Widget leaguesPage() {
-  Container(
-    alignment: Alignment.center,
-    color: Colors.lightBlue,
+Widget homePage(int steps, int goal) {
+  return Center(
+    child: ListView(
+      children: <Widget>[
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Padding(
+              padding: EdgeInsets.symmetric(vertical: 5),
+            ),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.calendar_today, color: Colors.grey.withOpacity(0.8),),
+                  new Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 2),
+                  ),
+                  new Text(
+                    "Today",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24, color: Colors.grey.withOpacity(1)),
+                  ),
+                ]
+            ),
+            homeIndicator(steps, goal),
+          ],
+        )
+      ],
+    ),
   );
+}
+
+
+Widget leaguesPage(String currentLeagueName, int steps, int goal, List<Widget> leaderboard, int totalSteps) {
+  return leaguesFocus(currentLeagueName, steps, goal, leaderboard, totalSteps);
+  
 }
 
 
